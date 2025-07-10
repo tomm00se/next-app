059749bd93fad62417730b54f99d2e59b80cb720
Tommy Bowden <tom@moose17.dev>
2025-07-10 14:24:14 +0100

    create client button component

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,10 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <button onClick={() => console.log("Im a button")}>Add To Cart</button>
+  );
+};
+
+export default AddToCart;
